6f344c8a8caaad718bcaaa644e316be8d73b2c12
Ondrej Hejda <ondrej.hejda@saviotechnics.com>
2019-11-04 15:11:28 +0100

    connect actions to buttons

--- toy_car.ino
+++ toy_car.ino
@@ -15,8 +15,9 @@
 
 #define BTN_LIGHT 17
 #define BTN_RIGHT 16
-#define BTN_ALARM 15
-#define BTN_LEFT 14
+#define BTN_LEFT  15
+#define BTN_ALARM 14
+#define BTN_HORN  12
 
 #define BTN_SAMPLING_MS 2
 #define BTN_STABLE_CNT 8
@@ -29,7 +30,12 @@ typedef struct {
   uint8_t input;
 } button_t;
 
-button_t buttons[4];
+#define BTNF_LIGHT 0
+#define BTNF_RIGHT 1
+#define BTNF_LEFT  2
+#define BTNF_ALARM 3
+#define BTNF_HORN  4
+button_t buttons[5];
 
 void btn_init(button_t *button, uint8_t btn_input) {
   button->input = btn_input;
@@ -62,67 +68,88 @@ void setup() {
   digitalWrite(RIGHT, OFF);
 
   pinMode(BTN_LIGHT, INPUT_PULLUP);
-  btn_init(&buttons[0], BTN_LIGHT);
+  btn_init(&buttons[BTNF_LIGHT], BTN_LIGHT);
   pinMode(BTN_RIGHT, INPUT_PULLUP);
-  btn_init(&buttons[1], BTN_RIGHT);
-  pinMode(BTN_ALARM, INPUT_PULLUP);
-  buttons[2].input = BTN_ALARM;
+  btn_init(&buttons[BTNF_RIGHT], BTN_RIGHT);
   pinMode(BTN_LEFT, INPUT_PULLUP);
-  buttons[3].input = BTN_LEFT;
+  btn_init(&buttons[BTNF_LEFT],  BTN_LEFT);
+  pinMode(BTN_ALARM, INPUT_PULLUP);
+  btn_init(&buttons[BTNF_ALARM], BTN_ALARM);
+  pinMode(BTN_HORN, INPUT_PULLUP);
+  btn_init(&buttons[BTNF_HORN], BTN_HORN);
 }
 
 void loop() {
   uint32_t now = millis();
-  
-  static bool btn_light = false;
-  static uint32_t btn_time = 0;
-  static uint8_t btn_light_cnt = 0;
-  
+
+  uint8_t blinker_status = 0;
+  uint8_t next_blinker_status = 0;
+
+  // read and filter buttons and connect actions
+  static uint32_t btn_time;
   if ((now - btn_time) >= BTN_SAMPLING_MS) {
     btn_time += BTN_SAMPLING_MS;
 
-    if (btn_filter(&buttons[0]))
-      digitalWrite(LIGHTS, !digitalRead(LIGHTS));
-    if (btn_filter(&buttons[1]))
+    if (btn_filter(&buttons[BTNF_LIGHT])) { // LIGHTS ON - OFF
+      static bool lights_on = false;
+      lights_on = !lights_on;
+      digitalWrite(LIGHTS, lights_on);
+    }
+    if (btn_filter(&buttons[BTNF_RIGHT])) { // BLINKER RIGHT
+      if (blinker_status == 0x1)
+        next_blinker_status = 0x0;
+      else if ((blinker_status == 0x0) || (blinker_status == 0x2))
+        next_blinker_status = 0x1;
+    }
+    if (btn_filter(&buttons[BTNF_LEFT])) { // BLINKER LEFT
+      if (blinker_status == 0x2)
+        next_blinker_status = 0x0;
+      else if ((blinker_status == 0x0) || (blinker_status == 0x1))
+        next_blinker_status = 0x2;
+    }
+    if (btn_filter(&buttons[BTNF_ALARM])) { // BLINKER ALARM
+      if (blinker_status == 0x3)
+        next_blinker_status = 0x0;
+      else
+        next_blinker_status = 0x3;
+    }
+    if (btn_filter(&buttons[BTNF_HORN])) // HORN
       startPlayback(horn_data, horn_length);
-
-  
-  digitalWrite(LED_BUILTIN, PLAYING);
-    
-    /*uint8_t raw_in = PINC & 0xF;
-    static uint8_t filter_in = 0xF;
-    uint8_t mask = 0x1;
-    static uint8_t filter_cnt[4];
-    for (int i=0; i<4; i++) {
-      if ((filter_in & mask) != (raw_in & mask)) {
-        filter_cnt[i] ++;
-        if (filter_cnt[i] >= BTN_STABLE_CNT)
-      }
-        
-      if (filter_in & (1<<i)
-    }
-    if (btn_light == digitalRead(BTN_LIGHT)) {
-      btn_light_cnt ++;
-      if (btn_light_cnt >= BTN_STABLE_CNT) {
-        btn_light = !btn_light;
-        if (btn_light == false)
-          digitalWrite(LIGHTS, !digitalRead(LIGHTS));
-      }
-    }
-    else
-      btn_light_cnt = 0;*/
   }
 
+  // notify active playback (debug)
+  digitalWrite(LED_BUILTIN, PLAYING);
+
+  // blink my blinker acording to blinker_status
   static uint32_t blink_t = 0;
-  if ((now - blink_t) > BLINK_PERIOD) {
-    blink_t += BLINK_PERIOD;
-    digitalWrite(LEFT, !digitalRead(LEFT));
-    digitalWrite(RIGHT, !digitalRead(RIGHT));
-    if (!PLAYING) {
-      if (!digitalRead(LEFT))
-        startPlayback(tick_data, tick_length);
-      else
-        startPlayback(tock_data, tock_length);
+  static bool blinker_on = false;
+  if ((blinker_status == 0) && (next_blinker_status != 0)) {
+    blinker_status = next_blinker_status;
+    blinker_on = true;
+    digitalWrite(RIGHT, blinker_status & 1);
+    digitalWrite(LEFT, blinker_status & 2);
+    if (!PLAYING) startPlayback(tock_data, tock_length);
+    blink_t = now;
+  }
+  else {
+    if ((now - blink_t) > BLINK_PERIOD) {
+      blink_t += BLINK_PERIOD;
+      if (blinker_on) {
+        blinker_on = false;
+        digitalWrite(LEFT, 0);
+        digitalWrite(RIGHT, 0);
+        if (!PLAYING) startPlayback(tick_data, tick_length);
+      }
+      else {
+        if (blinker_status != next_blinker_status)
+          blinker_status = next_blinker_status;
+        if (blinker_status != 0) {
+          blinker_on = true;
+          digitalWrite(RIGHT, blinker_status & 1);
+          digitalWrite(LEFT, blinker_status & 2);
+          if (!PLAYING) startPlayback(tock_data, tock_length);
+        }
+      }
     }
   }
 }
